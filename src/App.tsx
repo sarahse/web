97fb4b86f48a139284254a9af2bb638e49a6143e
Sarah Serussi <sarah.serussi@gmail.com>
2022-10-09 21:49:36 +0200

    add images

--- src/App.tsx
+++ src/App.tsx
@@ -13,59 +13,80 @@ const App = () => {
         </nav>
       </header>
       <main className="app__body">
-        <section className="about card" id="about">
+        <section className="about" id="about">
           <article className="text">
-            <h2 className="title">About</h2>
+            <h2 className="title">Hello, I'm Sarah 👋🏼</h2>
             <p>
-              Hello, I'm Sarah. I am a software developer working at Variant in
-              Trondheim. I like to knit, crochet, cook food for my daughter and
-              watch trash on TV. I believe there are too few women in
-              technology, and I try to do an extra effort in closing the gender
-              gap in the IT industry. I am part of the{" "}
-              <a href="https://tenknorge.com/">TENK</a> team in Trondheim where
-              we organize{" "}
+              I am a software developer working at Variant in Trondheim. I like
+              to knit, crochet, cook food for my daughter and watch trash on TV.
+              I believe there are too few women in technology, and I try to do
+              an extra effort in closing the gender gap in the IT industry. I am
+              part of the <a href="https://tenknorge.com/">TENK</a> team in
+              Trondheim where we organize{" "}
               <a href="https://tenknorge.com/tenk-tech-camp">TENK Tech Camp</a>{" "}
               which is a 2 day programming camp for 13-18 year old girls.
             </p>
           </article>
           <img
-            src="/media/DALL-E_2022-10-07_22.10.59_a_dream_within_a_dream_in_a_balloon_cyberpunk.png"
-            alt="A dream within a dream, DALL-E prompt"
+            src="/media/sarah-lilla.jpg"
+            alt="Sarah"
             className="clip-circle"
           />
         </section>
-        <section className="projects card" id="projects">
+        <section className="projects" id="projects">
           <h2>Projects</h2>
-          <h3>TENK Tech Camp</h3>
-          <p>
-            <a href="https://tenknorge.com/tenk-tech-camp">TENK Tech Camp</a> is
-            a 2 day programming camp for 13-18 year old girls in Norway. It is
-            run in three cities (as per 2022), and consists of workshops and
-            motivational talks which has the goal of inspiring more girls to
-            choose technology related studies and eventually work in the IT
-            industry.
-          </p>
-          <h3>Juno</h3>
-          <p>
-            I became a mom in 2021 and experienced the highs and lows of the
-            fourth trimester. I was often overwhelmed with the amount of
-            information and tasks needed to be done continuously in addition to
-            taking care of my daughter. I was missing an app that kept track of
-            all the things I needed to do in order to make my future self more
-            prepared and on top of things. In the first 6 months I was in a
-            bubble and couldn't really focus on anything other than my family of
-            three. After some time there were less sleepless nights and also
-            time to reflect and think of ideas for tools to enter this new life
-            as a first-time mom.
-          </p>
-          <p>
-            In May 2022 I was contacted by a doctor and an influencer with an
-            idea to make an app for moms to help them clear out the mess in
-            their head by organizing tasks, events, reminders, tips and hints
-            into a digital tool.
-          </p>
+          <article className="card">
+            <h3>TENK Tech Camp</h3>
+            <p>
+              <a href="https://tenknorge.com/tenk-tech-camp">TENK Tech Camp</a>{" "}
+              is a 2 day programming camp for 13-18 year old girls in Norway. It
+              is run in three cities (as per 2022), and consists of workshops
+              and motivational talks which has the goal of inspiring more girls
+              to choose technology related studies and eventually work in the IT
+              industry.
+            </p>
+          </article>
+          <article className="card">
+            <h3>Juno</h3>
+            <p>
+              I became a mom in 2021 and experienced the highs and lows of the
+              fourth trimester. I was often overwhelmed with the amount of
+              information and tasks needed to be done continuously in addition
+              to taking care of my daughter. I was missing an app that kept
+              track of all the things I needed to do in order to make my future
+              self more prepared and on top of things. In the first 6 months I
+              was in a bubble and couldn't really focus on anything other than
+              my family of three. After some time there were less sleepless
+              nights and also time to reflect and think of ideas for tools to
+              enter this new life as a first-time mom.
+            </p>
+            <p>
+              In May 2022 I was contacted by a doctor and an influencer with an
+              idea to make an app for moms to help them clear out the mess in
+              their head by organizing tasks, events, reminders, tips and hints
+              into a digital tool.
+            </p>
+          </article>
+          <article className="card">
+            <h3>Knitting and crocheting</h3>
+            <img
+              src="/media/DALL-E_2022-10-07_22.10.59_a_dream_within_a_dream_in_a_balloon_cyberpunk.png"
+              alt="DALL-E: A dream within a dream in a balloon."
+              className="clip-circle"
+            />
+            <img
+              src="/media/DALL-E_2022-10-07_22.11.09_a_dream_within_a_dream_in_a_balloon_2_cyberpunk.png"
+              alt="DALL-E: A dream within a dream in a balloon."
+              className="clip-circle"
+            />
+            <img
+              src="media/DALL-E_2022-10-07_22.10.24_a_dream_within_a_dream_in_an_ocean_cyberpunk.png"
+              alt="DALL-E: A dream within a dream in an ocean."
+              className="clip-circle"
+            />
+          </article>
         </section>
-        <section className="contact card" id="contact">
+        <section className="contact" id="contact">
           <h2>Contact</h2>
         </section>
       </main>
